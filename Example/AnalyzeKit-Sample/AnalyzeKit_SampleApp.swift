50af0b6a1963075af66c20704487197964c65caf
Michel-André Chirita <michel.chirita@gmail.com>
2024-10-14 17:58:49 +0200

    first commit

---
+++ Example/AnalyzeKit-Sample/AnalyzeKit_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalyzeKit_SampleApp.swift
+//  AnalyzeKit-Sample
+//
+//  Created by Michel-Andre Chirita on 14/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnalyzeKit_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
